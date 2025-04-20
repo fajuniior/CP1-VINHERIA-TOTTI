653fcc1969afefd4b5c6faf2081b27b524114535
Fabio <fabioprogerio@gmail.com>
2025-04-19 22:37:50 -0300

    Adicionando o ex2 JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,13 @@
-// 1 - Declaração de variáveis
+// 1) Declaração de variáveis
 let variavelUndefined;
 let variavelVazia = "";
 let variavelComValor = "Vinho Tinto";
 console.log("Undefined:", variavelUndefined);
 console.log("Vazia:", variavelVazia);
 console.log("Com valor:", variavelComValor);
+
+// 2) Operadores !=, ==, >=
+let x = 5, y = "5";
+console.log("x != y →", x != y);   // falso
+console.log("x == y →", x == y);   // verdadeiro
+console.log("x >= y →", x >= y);   // verdadeiro
