c7ebda78121044ee493f92f5113a0e6067b99ce7
Sebastian Iriarte Gonzales <sebasigonzales10@gmail.com>
2025-04-19 22:34:19 -0300

    adicionando ex1

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// 1 - Declaração de variáveis
+let variavelUndefined;
+let variavelVazia = "";
+let variavelComValor = "Vinho Tinto";
+console.log("Undefined:", variavelUndefined);
+console.log("Vazia:", variavelVazia);
+console.log("Com valor:", variavelComValor);
